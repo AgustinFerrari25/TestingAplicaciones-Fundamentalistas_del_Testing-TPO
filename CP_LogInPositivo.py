296b841c1d9fb6f3ea475c20f97e5884f85aaea1
Bautista Novarese <111317925+rubiolo3@users.noreply.github.com>
2022-11-29 00:43:36 -0300

    Add files via upload

---
+++ CP_LogInPositivo.py
@@ -0,0 +1,30 @@
+from selenium import webdriver 
+from selenium.webdriver.common.keys import Keys #Nos deja usar el ENTER
+import time
+from selenium.webdriver.common.by import By #Nos deja refereciar elementos dentro de la web
+
+PATH = "C:\Program Files (x86)\chromedriver.exe" 
+
+driver = webdriver.Chrome(PATH) #abrimos con chrome la pagina
+
+driver.get("https://magento.softwaretestingboard.com/") #link de la pagina
+print(driver.title) #muestre en pantalla
+
+search=driver.find_element(By.LINK_TEXT,"Sign In")
+search.send_keys(Keys.RETURN)
+IngresarEmail=driver.find_element(By.ID,"email")
+user="bnovarese@uade.edu.ar" #Usuario
+IngresarEmail.send_keys(user)
+IngresarContraseña=driver.find_element(By.ID,"pass")
+password="Fundamentalistastesting2022" #Contraseña
+IngresarContraseña.send_keys(password)
+IngresarContraseña.send_keys(Keys.RETURN)
+time.sleep(10)
+
+#driver.quit() #cierra pagina
+
+
+
+
+
+
